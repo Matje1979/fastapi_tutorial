ce496e15d1ef49ba6b9c38a73faba59e8c37a685
damir <damir.cicic@symphony.is>
2022-08-01 14:34:25 +0200

    Correcting database name

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from app.config import settings
 
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/fastapi_db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
